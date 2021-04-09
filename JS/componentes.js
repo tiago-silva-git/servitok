4789aaa0e67ec5ad6de095b39ad614b2d6d7a316
tiago-silva-git <tiago.silva.qa@gmail.com>
2021-04-08 21:03:54 -0300

    Teste copy reordenado disabled no js

--- JS/componentes.js
+++ JS/componentes.js
@@ -1,12 +1,11 @@
 /*Botão de copiar do modal*/
 /*TikTok*/
 function tiktok() {
-    document.getElementById("codtxt").disabled = true;
     var n = document.getElementById("codtxt");
     n.select();
     n.setSelectionRange(0, 99999)
     document.execCommand("copy");
+    document.getElementById("codtxt").disabled = true;
     var copiado = document.getElementById("btncopiar");
     copiado.innerHTML = `Copiado`
 }
-
